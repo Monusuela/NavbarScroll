6da419c1266d366a47a6511c8a177535d1c5da05
Mohinur <157112134+Monusuela@users.noreply.github.com>
2024-03-09 15:16:57 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+import { Fragment } from 'react';
+// import "./App.css"
+import Navigation from './components/Navigation';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Home from './pages/Home';
+import Posts from './pages/Posts';
+import About from "./pages/About"
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home/>
+    },
+    {
+      path: "posts",
+      element: <Posts />
+    },
+    {
+      path: "about",
+      element: <About />
+    } 
+   ], {
+    basename: "/"
+   })
+  return (
+    <Fragment>
+      <Navigation/>
+      <RouterProvider router={router} />
+    </Fragment>
+  );
+}
+
+export default App;
